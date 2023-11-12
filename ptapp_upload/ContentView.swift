2a8eb48770d8d1cd8fdbbcdc4e8fc7ee725d83f3
Cecilia Andreae <candreae@parktudor.org>
2023-11-12 14:25:13 -0500

    s

--- ptapp_upload/ContentView.swift
+++ ptapp_upload/ContentView.swift
@@ -1,85 +1,92 @@
 //
 //  ContentView.swift
-//  ptapp_upload
+//  photo
 //
-//  Created by Cecilia Andreae on 11/12/23.
+//  Created by Cecilia Andreae on 11/10/23.
 //
 
 import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    //show image pickker
+    @State var showImagePicker: Bool = false
+    
+    //show selected image
+    @State var selectedImage: Image? = Image("")
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack {
+            //create button to select image
+            Button(action: {
+                self.showImagePicker.toggle()
+            }, label: {
+                Text("select image")
+            })
+            
+            //show image
+            self.selectedImage?.resizable().scaledToFit()
+            
+            
+            //show button to upload image
+            Button(action:{
+                //convert image into base 64
+                
+                let uiImage :UIImage = self.selectedImage.asUIImage()
+                let imageData: Data = uiImage.jpegData(compressionQuality: 0.1) ?? Data()
+                let imageStr: String =
+                    imageData.base64EncodedString()
+                
+                //send request to server
+                guard let url: URL = URL(string:"http://parktudor.org.192-185-10-227.hgws18.hgwin.temp.domains/photos/") else {
+                    print("invalid URL")
+                    return
+                }
+                
+                //create parameters
+                let paramStr: String = "image=\(imageStr)"
+                let paramData: Data = paramStr.data(using:
+                        .utf8) ?? Data()
+                
+                
+                var urlRequest: URLRequest =
+                    URLRequest(url: url)
+                urlRequest.httpMethod = "POST"
+                urlRequest.httpBody = paramData
+                
+                //required for sending large data
+//                urlRequest.setValue("application/x-ww-form-urlencoded", forHTTPHeaderField: "Content-Type")
+                
+                //send request
+                URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                    guard let data = data else {
+                        print("invalid data")
+                        return
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+                    
+                    //show response in string
+                    let responseStr: String = String(data: data, encoding: .utf8) ?? ""
+                    print(responseStr)
+                })
+            
+            }, label: {
+                Text("Upload Image")
+            })
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .sheet(isPresented: $showImagePicker, content:
+            {
+            ImagePicker(image: self.$selectedImage)
+        })
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
